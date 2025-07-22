d12ba35fd36e775a2f4f61b94676fbad203bae1f
Andrés <kblandonv@unal.edu.co>
2025-07-22 06:53:33 -0500

    Add Docker Compose configuration and update Dockerfiles for frontend and backend services
    - Introduced docker-compose.yml to define services for backend and frontend.
    - Updated Dockerfile for backend to use python:3.10-slim-bullseye.
    - Created Dockerfile for frontend with Node.js setup and build instructions.
    - Added .dockerignore to exclude unnecessary files from the frontend build.
    - Enhanced main.py to mount static files and add CORS middleware.

--- main.py
+++ main.py
@@ -2,11 +2,27 @@
 
 from fastapi import FastAPI
 from api.search import router as search_router
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="Web Automation API",
     description="API para disparar la automatización Selenium",
     version="0.1.0"
+)
+
+# Monta 'downloads/' en /downloads
+app.mount(
+    "/downloads",
+    StaticFiles(directory="downloads"),
+    name="downloads"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Incluye los endpoints definidos en api/search.py
